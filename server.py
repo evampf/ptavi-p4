c65562d9b068a26c8fd6d3f0763ea04d30519075
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-10-24 15:40:38 +0200

    Apartado 6

--- server.py
+++ server.py
@@ -8,20 +8,27 @@ import socketserver
 import sys 
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
         print(self.client_address)
+        dicc = {}
+        print("El cliente nos manda: ", line.decode('utf-8'))
         for line in self.rfile:
-            print("El cliente nos manda: ", line.decode('utf-8'))
+            mensaje = line.decode('utf-8')
+            if mensaje[0] == 'REGISTER':
+            	dicc = {self.client_address[0]: self.client_address[1]}
+            	self.wfile.write(b'200 OK')
+            	print(dicc)
+
+
+
+
 
 if __name__ == "__main__":
 
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
