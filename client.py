35af4dac5a6529cd04de0799f94b8fe0e981be3e
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-10-24 15:40:25 +0200

    Apartado 6

--- client.py
+++ client.py
@@ -9,13 +9,21 @@ import sys
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = '.'.join(sys.argv[3:])
+LINE = ' '.join(sys.argv[3:])
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    Words = LINE.split(" ")
+    MSG = Words[0]
+    DIRECCION = Words[1]
+    EXPIRES = int(Words[2])
+    print(DIRECCION)
+    if MSG == "register":
+        Mensaje = ("REGISTER sip:" + DIRECCION + " SIP/2.0\r\n")
+        LINE = Mensaje
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
