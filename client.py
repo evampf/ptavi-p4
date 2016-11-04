9f60132515b6786b8bb3b62997e52486adf53ce1
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-11-04 11:52:55 +0100

    Terminado sin pep8

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -10,23 +8,21 @@ import sys
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
-    LINE = ' '.join(sys.argv[3:])
+    MSG_TYPE = sys.argv[3]
+    DIRECCTION = sys.argv[4]
+    EXPIRES = sys.argv[5]
+
 except:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
-
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    Words = LINE.split(" ")
-    MSG = Words[0]
-    DIRECCION = Words[1]
-    EXPIRES = int(Words[2])
-    print(DIRECCION)
-    if MSG == "register":
-        Mensaje = ("REGISTER sip:" + DIRECCION + " SIP/2.0\r\n")
-        LINE = Mensaje
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    if MSG_TYPE == "register":
+        Message = ("REGISTER sip:" + DIRECCTION + " SIP/2.0\r\n")
+        Message += ("Expires: " + EXPIRES + "\r\n\r\n")
+        print("Enviando:", Message)
+        my_socket.send(bytes(Message, 'utf-8'))
+
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
